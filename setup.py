bf1d8e1c51c610c72cb4557b89c54784d6c2f05a
Steffen Prince <steffen@sprin.io>
2015-11-27 12:26:19 -0800

    Add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -10,4 +10,21 @@ setup(
     description = "Render inline graphs with Markdown and Graphviz",
     license = "MIT",
     url = "https://github.com/sprin/markdown-inline-graphviz",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Developers',
+        'Topic :: Documentation',
+        'Topic :: Text Processing',
+
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
 )
