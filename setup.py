d9e043d48d1c8ef34768c38593a4d308f772c870
Steffen Prince <steffen@sprin.io>
2015-11-27 12:31:32 -0800

    Add publish command to setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,33 @@
+import os
+import sys
 from setuptools import setup
 
+VERSION = '1.0'
+
+if sys.argv[-1] == 'publish':
+    if os.system("pip freeze | grep wheel"):
+        print("wheel not installed.\nUse `pip install wheel`.\nExiting.")
+        sys.exit()
+    if os.system("pip freeze | grep twine"):
+        print("twine not installed.\nUse `pip install twine`.\nExiting.")
+        sys.exit()
+    os.system("python setup.py sdist bdist_wheel")
+    os.system("twine upload dist/*")
+    print("You probably want to also tag the version now:")
+    print("  git tag -a {0} -m 'version {0}'".format(VERSION))
+    print("  git push --tags")
+    sys.exit()
+
 setup(
-    name = "Markdown Inline Graphviz Extension",
-    version = "1.0",
-    py_modules = ["mdx_inline_graphviz"],
-    install_requires = ['Markdown>=2.3.1'],
-    author = "Steffen Prince",
-    author_email = "steffen@sprin.io",
-    description = "Render inline graphs with Markdown and Graphviz",
-    license = "MIT",
-    url = "https://github.com/sprin/markdown-inline-graphviz",
+    name="Markdown Inline Graphviz Extension",
+    version=VERSION,
+    py_modules=["mdx_inline_graphviz"],
+    install_requires=['Markdown>=2.3.1'],
+    author="Steffen Prince",
+    author_email="steffen@sprin.io",
+    description="Render inline graphs with Markdown and Graphviz",
+    license="MIT",
+    url="https://github.com/sprin/markdown-inline-graphviz",
     classifiers=[
         'Development Status :: 5 - Production/Stable',
 
